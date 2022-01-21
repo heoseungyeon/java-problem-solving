df42b30afac3846cf36ceb4403f16b6785788433
heoseungyeon <dia0312@naver.com>
2022-01-21 15:44:29 +0900

    refactor: 백준 17092 색칠 공부

--- src/baekjoon/Main_17092.java
+++ src/baekjoon/Main_17092.java
@@ -9,7 +9,8 @@ import java.util.*;
 /**
  * solved.ac bfs 17092 색칠 공부(1h30m) 풀이 방법: H와 W으로 2차원 배열을 만들면 Out of Memory 에러가 나기 때문에 배열의 크기를 줄이는
  * 아이디어가 중요하다. 검정색 부분을 리스트에 저장하고 9가지 경우에 대해 순회를 돌면서 hashMap의 key(String 으로 행과 열을 표현) 에 해당하는 값을 증가시켜
- * 이를 출력. 검정색의 갯수가 0인 경우는 전체 경우에서 카운트된 경우의 총합을 뺴서 계산
+ * 이를 출력. 검정색의 갯수가 0인 경우는 전체 경우에서 카운트된 경우의 총합을 뺴서 계산 리팩토링 : String으로 행과 열을 표현하는 것을 객체로 변경, 기존 Data
+ * 클래스에 equals & hashCode 를 Override 하여 구현
  */
 
 
@@ -19,7 +20,7 @@ public class Main_17092 {
     public static StringTokenizer tokens;
     public static int H, W, N;
     public static List<Data> points = new ArrayList<>();
-    public static Map<String, Integer> hashMap = new HashMap<>();
+    public static Map<Data, Integer> hashMap = new HashMap<>();
     public static int[] answer = new int[10];
 
     public static void main(String[] args) throws Exception {
@@ -46,9 +47,9 @@ public class Main_17092 {
             for (int j = 0; j < 3; j++) {
                 for (int k = 0; k < 3; k++) {
                     if (pRow + j >= 2 && pRow + j < H && pCol + k >= 2 && pCol + k < W) {
-                        hashMap.put(Integer.toString(pRow + j) + " " + Integer.toString(pCol + k),
+                        hashMap.put(new Data(pRow + j, pCol + k),
                             hashMap.getOrDefault(
-                                Integer.toString(pRow + j) + " " + Integer.toString(pCol + k), 0)
+                                new Data(pRow + j, pCol + k), 0)
                                 + 1);
                     }
                 }
@@ -56,8 +57,8 @@ public class Main_17092 {
         }
 
         int cnt = 0;
-        for (String string : hashMap.keySet()) {
-            answer[hashMap.get(string)] += 1;
+        for (Data data : hashMap.keySet()) {
+            answer[hashMap.get(data)] += 1;
             cnt += 1;
         }
 
@@ -107,6 +108,23 @@ public class Main_17092 {
                 "row=" + row +
                 ", col=" + col +
                 '}';
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) {
+                return true;
+            }
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
+            Data data = (Data) o;
+            return row == data.row && col == data.col;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(row, col);
         }
     }
 }
