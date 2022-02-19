dd94000950d126364fb272bcc67ee8249114494d
heoseungyeon <dia0312@naver.com>
2022-02-19 11:17:34 +0900

    fix: sql Kit! 누락 파일 재 업로드

---
+++ src/programmers/sqlKit/select/select_6.sql
@@ -0,0 +1,5 @@
+/*
+ 여러 기준으로 정렬하기 풀이 방법 : name은 오름차순, datetime은 내림차순이여야 하니 ORDER BY에 name 먼저 명세 후, datetime 조건 명세
+ */
+-- 코드를 입력하세요
+-- SELECT animal_id, name, datetime FROM animal_ins ORDER BY name asc , datetime desc;
