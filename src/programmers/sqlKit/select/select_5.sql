312d295d9ec550521257e7cbc3e96de97243e684
heoseungyeon <dia0312@naver.com>
2022-02-19 11:15:56 +0900

    solution: sql Kit!  여러 기준으로 정렬하기

--- src/programmers/sqlKit/select/select_5.sql
+++ src/programmers/sqlKit/select/select_5.sql
@@ -1,5 +1,5 @@
 /*
- 동물의 아이디와 이름 풀이 방법 : 모든 동울의 id, name 컬럼만 조회
+ 여러 기준으로 정렬하기 풀이 방법 : name은 오름차순, datetime은 내림차순이여야 하니 ORDER BY에 name 먼저 명세 후, datetime 조건 명세
  */
 -- 코드를 입력하세요
--- SELECT animal_id, name FROM animal_ins;
+-- SELECT animal_id, name, datetime FROM animal_ins ORDER BY name asc , datetime desc;
