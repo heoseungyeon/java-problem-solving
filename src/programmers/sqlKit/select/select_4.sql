c5d3f9f185320ee76f7743a4e095ee4c7b532659
heoseungyeon <dia0312@naver.com>
2022-02-19 11:09:42 +0900

    solution: sql Kit!  어린 동물 찾기

---
+++ src/programmers/sqlKit/select/select_4.sql
@@ -0,0 +1,5 @@
+/*
+ 어린 동물 찾기 풀이 방법 : 나이들지 않은 사람을 뽑는 거니 WHERE 절을 통해 INTAKE_CONDITION이 "Aged"가 아닌 것을 검색
+ */
+-- 코드를 입력하세요
+-- SELECT animal_id, name FROM animal_ins WHERE INTAKE_CONDITION != "Aged";
